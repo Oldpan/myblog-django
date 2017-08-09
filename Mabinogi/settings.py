78db91aa49af0b4b24fc68f473423fdcf1b289ed
Oldpan <295484914@qq.com>
2017-08-09 12:38:26 +0800

    Set a base_page with Navigation Bar

--- Mabinogi/settings.py
+++ Mabinogi/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'Mabinogi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "mabinogi",
+        'USER': "root",
+        'PASSWORD': 'Sina223317',
+        "HOST": '127.0.0.1'
     }
 }
 
@@ -106,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
