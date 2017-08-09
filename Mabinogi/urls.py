0963e80f3e760675a4fdcf50bc981bdc257ff535
Oldpan <295484914@qq.com>
2017-08-09 22:01:42 +0800

    Set a base_page and a app named main

--- Mabinogi/urls.py
+++ Mabinogi/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^$', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='base_html.html'), name='oldpan'),
+    url(r'^first_page$', TemplateView.as_view(template_name='first_page.html'), name='first_page'),
     url(r'^admin/', admin.site.urls),
 ]
